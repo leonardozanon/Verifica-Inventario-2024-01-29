3737b659036c3a133ffe95d1e8e44c43d9a1e773
leonardozanon <146086365+leonardozanon@users.noreply.github.com>
2024-01-30 21:51:40 +0100

    Update FormModifica.cs

--- FormModifica.cs
+++ FormModifica.cs
@@ -1,4 +1,4 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,11 +13,11 @@ namespace Verifica_Zanon_Leonardo_2024_01_29
 {
     public partial class FormModifica : Form
     {
-        public string id_utente;
-        public FormModifica()
+        public string id_componente;
+        public FormModifica(string id)
         {
             InitializeComponent();
-            id_utente = id_utente ;
+            id_componente = id;
             PopolaForm();
         }
         public void PopolaForm()
@@ -30,8 +30,7 @@ namespace Verifica_Zanon_Leonardo_2024_01_29
             try
             {
                 connessione.Open();
-                //string query = "SELECT * FROM utenti WHERE id = '" + id + "'";
-                string query = $"SELECT * FROM prodotti WHERE CodiceFarnell = '{id_utente}'";
+                string query = $"SELECT * FROM prodotti WHERE id = '{id_componente}'";
                 MySqlCommand cmd = new MySqlCommand(query, connessione);
                 MySqlDataReader dr = cmd.ExecuteReader();
 
@@ -41,7 +40,7 @@ namespace Verifica_Zanon_Leonardo_2024_01_29
                 textBoxProduttore.Text = dr.GetString("Produttore");
                 textBoxCodiceProduttore.Text = dr.GetString("CodiceProduttore");
                 textBoxQuantita.Text = dr.GetString("Quantita");
-                textBoxPrezzoUnitario.Text = dr.GetString("Prezzo");
+                textBoxPrezzoUnitario.Text = dr.GetDecimal("Prezzo").ToString();
 
                 dr.Close();
 
@@ -51,7 +50,7 @@ namespace Verifica_Zanon_Leonardo_2024_01_29
             {
                 MessageBox.Show("Impossibile stabilire una connessione al DB");
             }
-            
+
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
@@ -62,23 +61,23 @@ namespace Verifica_Zanon_Leonardo_2024_01_29
         private void buttonSave_Click(object sender, EventArgs e)
         {
             string ConnectionString = "server=127.0.0.1;" +
-                                    "database=anagrafica2iot;" +
+                                    "database=verificainventariozanon;" +
                                     "uid=root;";
             MySqlConnection connessione = new MySqlConnection(ConnectionString);
             try
             {
-                connessione.Open()
+                connessione.Open();
 
 
-                string query = $"UPDATE utenti " +
+                string query = $"UPDATE prodotti " +
                     $"SET " +
                     $"Nome = '{textBoxCodiceFarnell.Text}', " +
                     $"Cognome = '{textBoxDescrizione.Text}', " +
                     $"Cognome = '{textBoxProduttore.Text}', " +
                     $"Cognome = '{textBoxCodiceProduttore.Text}', " +
                     $"Cognome = '{textBoxQuantita.Text}', " +
-                    $"DataNascita = '{textBoxPrezzoUnitario.ToString()}' " +
-                    $"WHERE id = {id_utente}";
+                    $"DataNascita = '{textBoxPrezzoUnitario.Text}' " +
+                    $"WHERE id = {id_componente}";
                 MySqlCommand cmd = new MySqlCommand(query, connessione);
                 cmd.ExecuteNonQuery();
 
